78dd80c431494a50e46840207cede8d627b67e62
InesSousa11 <ines.gomes.sousa@outlook.com>
2025-05-24 17:32:04 +0100

    Added ASTBool and VBool classes, and changed parser to identify booleans.

---
+++ ASTBool.java
@@ -0,0 +1,13 @@
+class ASTBool implements ASTNode  {
+    boolean b;
+
+    ASTBool(boolean b0) {
+        b = b0;
+    }
+
+    public IValue eval(Environment<IValue> e) throws InterpreterError
+    {
+        return new VBool(b);
+    }
+
+}
